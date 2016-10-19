92998c4e5ff84c44aca8393f1d74244eb3865bae
Cenius Sanjaya <mc.sanjaya@gmail.com>
2016-10-19 16:54:12 +0700

    Delete Minggu1_awk.pl

--- Minggu1_awk.pl
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-awk 'BEGIN {print "Menampilkan baris kata"}'
-awk '{print $0}' makanan.csv
-awk 'BEGIN {print "Menampilkan kolom pertama"}'
-awk '{ print $1}' makanan.csv
-awk 'BEGIN {print "Menampilkan kolom kedua"}'
-awk '{print $2}' makanan.csv
-
