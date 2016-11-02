d92358b188c6a0e21835a8b47f52a9b047575606
Cenius Sanjaya <mc.sanjaya@gmail.com>
2016-11-02 19:37:50 +0700

    Delete Minggu8_python1.pl

--- Minggu8_python1.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-prices = {'applepen': 0.40, 'banana': 0.50}
-my_purchase = {
-    'applepen': 1,
-    'banana': 6}
-grocery_bill = sum(prices[fruit] * my_purchase[fruit]
-                   for fruit in my_purchase)
-print 'I owe the grocer $%.2f' % grocery_bill
